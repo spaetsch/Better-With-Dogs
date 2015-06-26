0f14d3773051db14f1e9ea674db9a44a530b78d6
Sarah Paetsch <spaetsch@gmail.com>
2015-06-25 20:00:06 -0700

    Improve style, clean up code

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,6 @@
 
 $('#search-form').submit(function(event) {
   var $flickrSearch = $("#flickr-search").val();
-  console.log("sending search tag:", $flickrSearch);
-
   var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
   var options = {
     tags: $flickrSearch,
@@ -15,55 +13,26 @@ $('#search-form').submit(function(event) {
 
   function showPhotosLeft(data) {
     var firstPhoto = '<a href="' + data.items[0].link + '"><img src="' + data.items[0].media.m + '"></a>';
-    // var photoList = '<ul>';
-    // $.each(data.items, function(i, photo) {
-    //   photoList += '<li>';
-    //   photoList += '<a href="' + photo.link + '">';
-    //   photoList += '<img src="' + photo.media.m + '"></a></li>';
-    // });
-    // photoList += '</ul>';
-    // $('#left1').html(photoList);
-     $('#left1').html(firstPhoto);
-
+    $('#withHeader').html($flickrSearch + " with dogs");
+    $('#left1').html(firstPhoto);
   }
 
   function showPhotosRight(data) {
-       var firstDogPhoto = '<a href="' + data.items[0].link + '"><img src="' + data.items[0].media.m + '"></a>';
-     $('#right1').html(firstDogPhoto);
-
+    var firstDogPhoto = '<a href="' + data.items[0].link + '"><img src="' + data.items[0].media.m + '"></a>';
+    $('#withoutHeader').html($flickrSearch + " without dogs");
+    $('#right1').html(firstDogPhoto);
   }
 
   event.preventDefault();
   $.getJSON(flickrAPI, options, showPhotosLeft);
   $.getJSON(flickrAPI, optionsWDog, showPhotosRight);
-
-
 });
 
 
+$("#withDogs").click(function(event){
+  console.log("you have a soul")
+});
 
-// $('#search-formDog').submit(function(event) {
-//   var $flickrSearchDog = $("#flickr-searchDog").val() + " ,dog";
-
-//   console.log("sending search tags:", $flickrSearchDog);
-
-//   var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-//   var options = {
-//     tags: $flickrSearchDog,
-//     format: "json"
-//   };
-//   function showPhotos(data) {
-//     var photoList = '<ul>';
-//     $.each(data.items, function(i, photo) {
-//       photoList += '<li>';
-//       photoList += '<a href="' + photo.link + '">';
-//       photoList += '<img src="' + photo.media.m + '"></a></li>';
-//     });
-//     photoList += '</ul>';
-//     $('#search-resultsDog').html(photoList);
-//   }
-
-//   event.preventDefault();
-
-//   $.getJSON(flickrAPI, options, showPhotos);
-// });
+$("#withoutDogs").click(function(event){
+  console.log("you are soulless and empty")
+});

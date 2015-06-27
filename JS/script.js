59c1e86cd4a271a1955781b5a0c7c6fb6d1424cf
Sarah Paetsch <spaetsch@gmail.com>
2015-06-26 20:54:39 -0700

    Add highlight toggle on vote. Add randomization of results.

--- JS/script.js
+++ JS/script.js
@@ -10,32 +10,32 @@ $('#search-form').submit(function(event) {
     tags: $flickrSearch + " ,dog",
     format: "json"
   };
-
-  function showPhotosLeft(data) {
-    var firstPhoto = '<a href="' + data.items[0].link + '"><img src="' + data.items[0].media.m + '"></a>';
+  function showPhotosDog(data) {
+    var nextDog = Math.floor(Math.random() * 20) + 1;
+    var firstDogPhoto = '<a href="' + data.items[nextDog].link + '"><img src="' + data.items[nextDog].media.m + '"></a>';
     $('#withHeader').html($flickrSearch + " with dogs");
-    $('#left1').html(firstPhoto);
+    $('#photo-dog').html(firstDogPhoto);
   }
-
-  function showPhotosRight(data) {
-    var firstDogPhoto = '<a href="' + data.items[0].link + '"><img src="' + data.items[0].media.m + '"></a>';
+  function showPhotosNoDog(data) {
+    var nextItem = Math.floor(Math.random() * 20) + 1;
+    var firstPhoto = '<a href="' + data.items[nextItem].link + '"><img src="' + data.items[nextItem].media.m + '"></a>';
     $('#withoutHeader').html($flickrSearch + " without dogs");
-    $('#right1').html(firstDogPhoto);
+    $('#photo-nodog').html(firstPhoto);
   }
-
   event.preventDefault();
-  $.getJSON(flickrAPI, options, showPhotosLeft);
-  $.getJSON(flickrAPI, optionsWDog, showPhotosRight);
+  $.getJSON(flickrAPI, options, showPhotosNoDog);
+  $.getJSON(flickrAPI, optionsWDog, showPhotosDog);
 });
 
 
 $("#withDogs").click(function(event){
   console.log("you have a soul");
-  $('#verdict').html("Dogs rule!");
-
+  $("#photo-dog").addClass("highlight");
+  $("#photo-nodog").removeClass("highlight");
 });
 
 $("#withoutDogs").click(function(event){
   console.log("you are soulless and empty");
-  $('#verdict').html("Dogs drool...");
+  $("#photo-nodog").addClass("highlight");
+  $("#photo-dog").removeClass("highlight");
 });
